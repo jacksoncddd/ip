9df8513b6095c48599760ba0e829e9622c3a1e04
Jackson Chia <chiadong07@gmail.com>
2026-01-25 03:57:27 +0800

    rename, greet, exit

---
+++ src/main/java/Trax.java
@@ -0,0 +1,9 @@
+public class Trax {
+    public static void main(String[] args) {
+
+        System.out.println("Hello! I'm Trax");
+        System.out.println("What can I do for you?\n");
+        System.out.println("Bye. Hope to see you again soon!");
+
+    }
+}
