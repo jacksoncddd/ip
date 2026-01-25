0b0f01755226190f175f205b6590fb6c27d0bc0b
Jackson Chia <chiadong07@gmail.com>
2026-01-26 03:36:07 +0800

    add, list

--- src/main/java/Trax.java
+++ src/main/java/Trax.java
@@ -1,10 +1,10 @@
-import java.util.Scanner;
+import java.util.*;
 
 
 public class Trax {
     public static void main(String[] args) {
 
-        //String[] tasks =  new String[100];
+        ArrayList<String> tasks = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Hello! I'm Trax");
@@ -16,8 +16,15 @@ public class Trax {
             if (input.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if (input.equals("list")) {
+                for(int i = 1; i<= tasks.size(); i++) {
+                    System.out.println(String.format("%d. %s", i, tasks.get(i-1)));
+                }
+            } else {
+                tasks.add(input);
+                System.out.println("added: " + input);
             }
-            System.out.println(input);
+
         }
 
     }
