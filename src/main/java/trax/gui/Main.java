f9eef2a5a1eb97e2970e5bf2e113f77bb1bbdf2b
Jackson Chia <chiadong07@gmail.com>
2026-02-16 01:13:55 +0800

    Add GUI using JavaFX

---
+++ src/main/java/trax/gui/Main.java
@@ -0,0 +1,28 @@
+package trax.gui;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import trax.main.Trax;
+
+public class Main extends Application {
+
+    private Trax trax = new Trax();
+
+    @Override
+    public void start(Stage stage) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
+            AnchorPane ap = fxmlLoader.load();
+            Scene scene = new Scene(ap);
+            stage.setScene(scene);
+            fxmlLoader.<MainWindow>getController().setTrax(trax);  // inject the Duke instance
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
