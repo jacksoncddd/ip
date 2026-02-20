04356ffe67dd86c7ebe3bdb65ba4d330c225d0f4
Jackson Chia <chiadong07@gmail.com>
2026-02-20 22:49:28 +0800

    Upgrade GUI

--- src/main/java/trax/gui/Main.java
+++ src/main/java/trax/gui/Main.java
@@ -15,6 +15,8 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) {
         try {
+            stage.setTitle("Trax Hotline!");
+
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
